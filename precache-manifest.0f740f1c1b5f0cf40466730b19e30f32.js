7a99aa305d871b9cebbdc9f2db8a33cb0345efe5
SahilMund <53625989+SahilMund@users.noreply.github.com>
2020-10-08 10:23:05 +0530

    Updates

---
+++ precache-manifest.0f740f1c1b5f0cf40466730b19e30f32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687eaa217422a0ef5b930601eb0c7b7e",
+    "url": "/React-task-manager/index.html"
+  },
+  {
+    "revision": "b1c7106f694ccefc7f90",
+    "url": "/React-task-manager/static/css/main.126dff07.chunk.css"
+  },
+  {
+    "revision": "f9b57988bc1a36f77bf6",
+    "url": "/React-task-manager/static/js/2.e4673994.chunk.js"
+  },
+  {
+    "revision": "b1c7106f694ccefc7f90",
+    "url": "/React-task-manager/static/js/main.ad24d53d.chunk.js"
+  },
+  {
+    "revision": "3312672d9c05b8bfdf52",
+    "url": "/React-task-manager/static/js/runtime-main.76a4a32b.js"
+  }
+]);
